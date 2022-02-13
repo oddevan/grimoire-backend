620958fccda65f7f4af07ada10fd6f3ec421c1c6
Evan Hildreth <me@eph.me>
2022-02-12 22:27:37 -0500

    Add composer and coding standards

--- functions.php
+++ functions.php
@@ -1,6 +1,23 @@
 <?php
+/**
+ * Entry point for Grimoire custom code.
+ *
+ * @package oddevan\Grimoire
+ */
 
-add_filter("pods_admin_capabilities", function ($pods_admin_capabilities, $cap) {
-	$pods_admin_capabilities[] = "administrator";
-	return $pods_admin_capabilities;
-}, 10, 2);
+namespace oddEvan\Grimoire;
+
+$grimoire_config_file = __DIR__ . '/grimoire-config.php';
+if ( file_exists( $grimoire_config_file ) ) {
+	include_once $grimoire_config_file;
+}
+
+add_filter(
+	'pods_admin_capabilities',
+	function ( $pods_admin_capabilities, $cap ) {
+		$pods_admin_capabilities[] = 'administrator';
+		return $pods_admin_capabilities;
+	},
+	10,
+	2
+);
